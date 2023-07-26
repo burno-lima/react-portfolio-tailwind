9ad41cc8ec7cc38651819ed62fdc423fce262fab
Bruno Lima <brunoluislima19@gmail.com>
2023-07-26 00:24:34 -0300

    image dev-ed-wave

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import {
   AiFillInstagram,
   AiFillLinkedin,
   AiFillYoutube
-} from "react-icons/ai"
+} from "react-icons/ai";
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
 
 export default function Home() {
   return (
@@ -18,18 +20,21 @@ export default function Home() {
             <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-4 rounded-md ml-8" href="#">Resume</a></li>
           </ul>
         </nav>
-        <div>
-          <h2>Bruno Lima</h2>
-          <h3>Developer</h3>
-          <p>              
+        <div className="text-center p-10">
+          <h2 className="text-5xl py-2 text-teal-600 font-medium">Bruno Lima</h2>
+          <h3 className="text-2xl py-2">Developer</h3>
+          <p className="text-md py-5 leading-8 text-gray-800">              
             Freelancer providing services for programming and design content
             needs. Join me down below and let's get cracking!
           </p>
         </div>
-        <div>
+        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
           <AiFillInstagram />
           <AiFillLinkedin />
           <AiFillYoutube />
+        </div>
+        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+          <Image src={deved} layout="fill" objectFit="cover"/>
         </div>
       </section>
     </main>
