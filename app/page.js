132fb4c6bdb762480bbc420fe65d16c49224259e
Bruno Lima <brunoluislima19@gmail.com>
2023-07-25 23:39:54 -0300

    personalize button and font burtons

--- app/page.js
+++ app/page.js
@@ -5,12 +5,12 @@ export default function Home() {
     <main className="bg-white px-10">
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between">
-          <h1 className="text-2xl">developerdbyed</h1>
+          <h1 className="text-xl font-burtons">developerdbyed</h1>
           <ul className="flex items-center">
             <li>
-              <BsFillMoonStarsFill />
+              <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
             </li>
-            <li><a href="#">Resume</a></li>
+            <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-4 rounded-md ml-8" href="#">Resume</a></li>
           </ul>
         </nav>
       </section>
