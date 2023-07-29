7fcdb0c780cbcc81b1d4bd40f2b4ec1b3bcbfabf
Bruno Lima <brunoluislima19@gmail.com>
2023-07-29 10:40:08 -0300

    theme dark

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import {
   AiFillInstagram,
@@ -15,42 +16,46 @@ import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
-
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <main className="bg-white px-10">
+    <div className={darkMode ? "dark" : ""}>
+    <main className="bg-white dark:bg-gray-900 px-10 md:px-20 lg:px-40">
       <section className="min-h-screen">
-        <nav className="py-10 mb-12 flex justify-between">
+        <nav className="py-10 mb-12 flex justify-between dark:text-white">
           <h1 className="text-xl font-burtons">developerdbyed</h1>
           <ul className="flex items-center">
             <li>
-              <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+              <BsFillMoonStarsFill 
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer text-2xl"/>
             </li>
             <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-4 rounded-md ml-8" href="#">Resume</a></li>
           </ul>
         </nav>
         <div className="text-center p-10">
-          <h2 className="text-5xl py-2 text-teal-600 font-medium">Bruno Lima</h2>
-          <h3 className="text-2xl py-2">Developer</h3>
-          <p className="text-md py-5 leading-8 text-gray-800">              
+          <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">Bruno Lima</h2>
+          <h3 className="text-2xl py-2 md:text-3xl dark:text-white">Developer</h3>
+          <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200">
             Freelancer providing services for programming and design content
             needs. Join me down below and let's get cracking!
           </p>
         </div>
-        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600  dark:text-gray-400">
           <AiFillInstagram />
           <AiFillLinkedin />
           <AiFillYoutube />
         </div>
-        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden mt-20 md:h-96 md:w-96">
           <Image src={deved} layout="fill" objectFit="cover"/>
         </div>
       </section>
       <section>
         <div>
-          <h3 className="text-3xl py-1">Services I offer</h3>
-          <p className="text-md py-2 leading-8 text-gray-800">
+          <h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
+          <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
             Since the beginning of my joutney as a Freelancer
             developer, I've done remote work for
             <span className="text-teal-500"> agencies </span>
@@ -58,7 +63,7 @@ export default function Home() {
             and collaborated with talanted people to create digite
             for both businees and consumer use.
           </p>
-          <p className="text-md py-2 leading-8 text-gray-80">
+          <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
             I offer from a wide range of services, including
             programming and teaching.
           </p>
@@ -173,5 +178,6 @@ export default function Home() {
         </div>
       </section>
     </main>
+    </div>
   )
 }
